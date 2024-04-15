45be60fcf8ef047d1fbb7de187263f76e449f8d3
awb324 <awb324@lehigh.edu>
2024-04-15 01:51:30 -0400

    comm

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-  }
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+function showStats() {
+    var dropdown = document.getElementById("stats-dropdown");
+    var selectedValue = dropdown.value;
+    var statsTable = document.getElementById("stats-table");
+
+    
+    statsTable.innerHTML = "";
+
+    
+    if (selectedValue == "1") {
+        
+        statsTable.innerHTML = "<table><tr><th>Appearances</th><th>Goals</th><th>Assists</th></tr><tr><td>25</td><td>10</td><td>8</td></tr></table>";
+    } else if (selectedValue == "2") {
+        
+        statsTable.innerHTML = "<table><tr><th>Appearances</th><th>Goals</th><th>Assists</th></tr><tr><td>30</td><td>15</td><td>12</td></tr></table>";
+    }
+  
+}
